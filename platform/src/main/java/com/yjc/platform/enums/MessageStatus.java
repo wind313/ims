a1d04b821b60b0ac07899a5a3f4c45f369a9db76
wind313 <3136002710@qq.com>
2024-05-10 22:26:07 +0800

    2

--- platform/src/main/java/com/yjc/platform/enums/MessageStatus.java
+++ platform/src/main/java/com/yjc/platform/enums/MessageStatus.java
@@ -8,7 +8,9 @@ public enum MessageStatus {
 
     UNREAD(0,"未读"),
     ALREADY_READ(1,"已读"),
-    RECALL(2,"已撤回");
+    RECALL(2,"已撤回"),
+    SEND_DELETE(3,"发送者已删除"),
+    RECEIVE_DELETE(4,"接收者已删除");
 
     private Integer code;
     private String desc;
